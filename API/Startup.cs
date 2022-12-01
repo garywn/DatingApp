74f5dccf4cf31647599d6e99752a5daaefc585a3
garywn <109641271+garywn@users.noreply.github.com>
2022-12-01 13:27:59 -0600

    Section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -19,27 +19,19 @@ namespace API
     public class Startup
     {
         private readonly IConfiguration _config;
-       
         public Startup(IConfiguration config)
         {
             _config = config;
-           
         }
-
-
-
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddDbContext<DataContext>(options =>
             {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
-
             services.AddControllers();
-            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
@@ -60,7 +52,9 @@ namespace API
 
             app.UseRouting();
 
-            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
